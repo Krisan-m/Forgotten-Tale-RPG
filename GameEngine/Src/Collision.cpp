28836e0bc276ba44ad484506847be9a2df47f628
Krisan Murugendran <km.krisan@gmail.com>
2018-10-14 17:41:32 -0400

    Implement TileComponent class

--- GameEngine/Src/Collision.cpp
+++ GameEngine/Src/Collision.cpp
@@ -1,5 +1,6 @@
 #include "Collision.h"
 #include "SDL.h"
+#include "ECS\ColliderComponent.h"
 
 bool Collision::AABB(const SDL_Rect& recA, const SDL_Rect& recB)
 {
@@ -14,4 +15,17 @@ bool Collision::AABB(const SDL_Rect& recA, const SDL_Rect& recB)
 	}
 
 	return false;
+}
+
+bool Collision::AABB(const ColliderComponent& colA, const ColliderComponent& colB)
+{
+	if (AABB(colA.collider, colB.collider))
+	{
+		std::cout << colA.tag << "hit: " << colB.tag << std::endl;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
