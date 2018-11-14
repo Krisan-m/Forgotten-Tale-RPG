533c603c0cfecf3656d8cab7b72b95d274401f1a
Krisan Murugendran <km.krisan@gmail.com>
2018-11-14 18:05:33 -0500

    Fix issue with erasing dialogue

--- GameEngine/Src/ECS/UILabel.h
+++ GameEngine/Src/ECS/UILabel.h
@@ -26,8 +26,9 @@ public:
 
 	void SetLabelText(std::string text, std::string font)
 	{
-		std::string displayString = text.substr(0, progress/10);
-		if(progress != text.length()*10) progress++;
+		labelText = text;
+		std::string displayString = text.substr(0, progress/5);
+		if(progress != text.length()*5) progress++;
 
 		SDL_Surface* surf = TTF_RenderText_Blended_Wrapped(Game::assets->GetFont(font), displayString.c_str(), textColour, 500);
 		labelTexture = SDL_CreateTextureFromSurface(Game::renderer, surf);
@@ -39,14 +40,14 @@ public:
 
 	void draw() override
 	{
-		if (progress != labelText.length()*10 + 10) SetLabelText(labelText, labelFont);
+		if (progress != labelText.length()*5 + 5) SetLabelText(labelText, labelFont);
 
 		SDL_RenderCopy(Game::renderer, labelTexture, nullptr, &position); //nullptr for source rect since we want to draw all of it
 	}
 
 private:
 	SDL_Rect position;
-	int progress = 10;
+	int progress = 5;
 	std::string labelText;
 	std::string labelFont;
 	SDL_Color textColour;
